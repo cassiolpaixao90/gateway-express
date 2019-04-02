47e2336e9705da760840bff869a2902f400a7a36
cassio.paixao <cassio.paixao@pe.senai.br>
2019-04-02 04:40:44 -0300

    add jwt and oauth2 suport

--- server.js
+++ server.js
@@ -1,6 +1,43 @@
+require('console.table');
 const path = require('path');
 const gateway = require('express-gateway');
+const services = require('express-gateway/lib/services');
 
-gateway()
-  .load(path.join(__dirname, 'config'))
-  .run();
+services.user
+  .insert({
+    username: 'bar',
+    firstname: 'foo',
+    lastname: 'foobar',
+    email: 'bar@foo.com'
+  })
+  .then(user => {
+    console.table({ username: user.username, id: user.id });
+    return Promise.all([
+      user,
+      services.application.insert(
+        {
+          name: 'my-app',
+          redirectUri: 'http://example.com'
+        },
+        user.id
+      )
+    ]);
+  })
+  .then(([user, app]) => {
+    console.table([
+      { id: app.id, name: app.name, redirectUri: app.redirectUri }
+    ]);
+
+    return Promise.all([
+      services.credential.insertCredential(user.id, 'basic-auth'),
+      services.credential.insertCredential(app.id, 'oauth2')
+    ]);
+  })
+  .then(credentials => {
+    console.table(
+      credentials.map(c => ({ id: c.id, password: c.password || c.secret }))
+    );
+    gateway()
+      .load(path.join(__dirname, 'config'))
+      .run();
+  });
